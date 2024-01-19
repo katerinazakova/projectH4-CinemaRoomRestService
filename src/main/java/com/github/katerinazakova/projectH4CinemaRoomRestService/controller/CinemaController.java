4c4dec02dc9f9348a383e1622475ac347c950077
Katerina Zakova <zakova.kz@gmail.com>
2024-01-19 10:00:45 +0100

    feat(CinemaController.java): implement /refund endpoint to POST method

--- src/main/java/com/github/katerinazakova/projectH4CinemaRoomRestService/controller/CinemaController.java
+++ src/main/java/com/github/katerinazakova/projectH4CinemaRoomRestService/controller/CinemaController.java
@@ -1,13 +1,11 @@
 package com.github.katerinazakova.projectH4CinemaRoomRestService.controller;
 
 import com.github.katerinazakova.projectH4CinemaRoomRestService.entity.Seats;
+import com.github.katerinazakova.projectH4CinemaRoomRestService.entity.Ticket;
 import com.github.katerinazakova.projectH4CinemaRoomRestService.service.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -28,6 +26,11 @@ public class CinemaController {
 
     @PostMapping("/purchase")
     public ResponseEntity<Object> buyCinemaTicket(@RequestBody Seats seat) {
-        return cinemaService.buyTicket(seat.getRow(), seat.getColumn());
+        return cinemaService.bookingTicket(seat.getRow(), seat.getColumn());
+    }
+
+    @PostMapping ("/return")
+    public ResponseEntity<Object> refundTicket (@RequestBody Ticket ticket){
+        return cinemaService.refundOfPurchaseTicket(ticket.getToken());
     }
 }
